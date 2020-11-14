027c4935f2d5a4d93f31c512757003e2b9dc55a9
Cat Meza <musical_kitty@hotmail.com>
2020-11-14 11:01:28 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const ExpressError = require('./services/expressError');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+
+
+
+
+
+app.use(function (req, res, next) {
+    const notFoundError = new ExpressError("Not Found", 404);
+    return next(notFoundError)
+});
+
+
+app.use(function(err, req, res, next) {
+    let status = err.status || 500;
+    let message = err.message;
+  
+    return res.status(status).json({
+      error: {message, status}
+    });
+  });
+
+
+module.exports = app;
