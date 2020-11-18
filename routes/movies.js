38b11f061f1c1ae82eb105d03017c41d91911998
Cat Meza <musical_kitty@hotmail.com>
2020-11-18 09:39:50 -0700

    small changes to css

--- routes/movies.js
+++ routes/movies.js
@@ -3,6 +3,7 @@ const router = new express.Router();
 const ExpressError = require('../services/expressError');
 const {optionsAll, getMovies} = require('../services/movieDatabase');
 const {optionsSingle, getMovieDetail} = require('../services/movieDatabase');
+const {getPoster, imdbCode} = require('../services/omdb');
 const {getRating} = require('../database/movieDB');
 
 
@@ -12,13 +13,14 @@ router.get('/', (req,res,next) => {
 
 router.post('/', async (req,res,next) => {  
     try {
-        optionsAll.params['s'] = req.body.search;    
+        optionsAll.params['s'] = req.body.search; 
+        search = req.body.search;   
         const movies = await getMovies();
         if (movies.Response == 'False') {
             const notFoundError = new ExpressError("Nothing Found. Try searching for something else.", 404);
             return next(notFoundError)
         };
-        res.render('movie', { movies : movies })
+        res.render('movie', { movies : movies, search : search })
     }  
     catch (err) {
         return next(err)
@@ -35,7 +37,10 @@ router.get('/:imdbID', async (req,res,next) => {
             const notFoundError = new ExpressError("Movie Not Found. Try searching for something else.", 404);
             return next(notFoundError)
         };
-        res.render('movieDetail', { movie : movie, rating : rating })
+        imdbCode.i = String(req.params.imdbID);
+        const poster = await getPoster();
+
+        res.render('movieDetail', { movie : movie, rating : rating , poster : poster})
     } 
     catch (err) {
         return next(err)
